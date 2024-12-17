0cf28f574185812eb2bc867f26bf30a9272a3288
singamet <ametojsingh007@gmail.com>
2024-12-17 22:41:20 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const foodRoutes = require('./routes/foodRoutes')
 const userRoutes = require('./routes/userRoutes')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
